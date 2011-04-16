6adf06d561377beae83e84619af7b679df22a44c
bgmeiner <bgmeiner@1541af92-db4b-9b20-5331-2e99740d2ad4>
2011-04-16 15:49:30 +0000

    - Added bin folder to svn ignore list
    git-svn-id: https://wifiremote.googlecode.com/svn/trunk@30 1541af92-db4b-9b20-5331-2e99740d2ad4

--- bin/Release/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST WifiRemote_UNMERGED.dll del WifiRemote_UNMERGED.dll 
-ren WifiRemote.dll WifiRemote_UNMERGED.dll 
-ilmerge /ndebug /out:WifiRemote.dll WifiRemote_UNMERGED.dll ZeroconfService.dll Newtonsoft.Json.dll
