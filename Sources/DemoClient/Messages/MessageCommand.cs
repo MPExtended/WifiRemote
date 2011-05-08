e0d5bfe599a19f296623663dc4a8f17c49fa92b8
shukuyen@googlemail.com <shukuyen@googlemail.com@1541af92-db4b-9b20-5331-2e99740d2ad4>
2011-05-08 17:13:55 +0000

    Moved to correct folder and added to wifiremote solution
    git-svn-id: https://wifiremote.googlecode.com/svn/trunk@70 1541af92-db4b-9b20-5331-2e99740d2ad4

---
+++ Sources/DemoClient/Messages/MessageCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoClient
+{
+    class MessageCommand : IMessage
+    {
+        string type = "command";
+        public string Type
+        {
+            get { return type; }
+        }
+
+        public String Command { get; set; }
+    }
+}
