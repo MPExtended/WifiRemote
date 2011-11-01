b87408eed878b3f7dcc08c93d848ad88607f4908
shukuyen@googlemail.com <shukuyen@googlemail.com@1541af92-db4b-9b20-5331-2e99740d2ad4>
2011-11-01 14:33:12 +0000

    rename part 2 ;)
    git-svn-id: https://wifiremote.googlecode.com/svn/trunk@163 1541af92-db4b-9b20-5331-2e99740d2ad4

---
+++ Sources/WifiRemote/MpExtended/MpExtendedMediaTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WifiRemote.MpExtended
+{
+    /// <summary>
+    /// MediaTypes for mapping 
+    /// </summary>
+    public enum MpExtendedMediaTypes
+    {
+        Movie = 0,
+        MusicTrack = 1,
+        Picture = 2,
+        TVEpisode = 3,
+        File = 4,
+        TVShow = 5,
+        TVSeason = 6,
+        MusicAlbum = 7,
+        MusicArtist = 8
+    }
+}
