f715fd5264bb2e3cd4fe06422d59e246db96ed54
shukuyen@googlemail.com <shukuyen@googlemail.com@1541af92-db4b-9b20-5331-2e99740d2ad4>
2013-09-19 17:20:12 +0000

    Increased server version to 13
    git-svn-id: https://wifiremote.googlecode.com/svn/trunk@218 1541af92-db4b-9b20-5331-2e99740d2ad4

--- Sources/WifiRemote/Messages/MessageWelcome.cs
+++ Sources/WifiRemote/Messages/MessageWelcome.cs
@@ -10,7 +10,7 @@ namespace WifiRemote
     class MessageWelcome : IMessage
     {
         string type = "welcome";
-        int server_version = 12;
+        int server_version = 13;
         AuthMethod authMethod = AuthMethod.UserPassword;
 
         /// <summary>
