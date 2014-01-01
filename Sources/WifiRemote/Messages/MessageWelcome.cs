57b2f1dd45f74d1f6acbcc70f22406dea91f4b81
Shukuyen <shukuyen@googlemail.com>
2014-01-01 22:14:21 +0100

    Increased server version to 14 due to new screenshot command and message

--- Sources/WifiRemote/Messages/MessageWelcome.cs
+++ Sources/WifiRemote/Messages/MessageWelcome.cs
@@ -10,7 +10,7 @@ namespace WifiRemote
     class MessageWelcome : IMessage
     {
         string type = "welcome";
-        int server_version = 13;
+        int server_version = 14;
         AuthMethod authMethod = AuthMethod.UserPassword;
 
         /// <summary>
