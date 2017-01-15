cc91f92ebd9780123bd722bc83265c8d6b644bf4
Shukuyen <shukuyen@googlemail.com>
2017-01-15 21:34:15 +0100

    Bumper version number to 0.8.3.1

--- Sources/WifiRemote/Properties/AssemblyInfo.cs
+++ Sources/WifiRemote/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using MediaPortal.Common.Utils;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.3.0")]
-[assembly: AssemblyFileVersion("0.8.3.0")]
+[assembly: AssemblyVersion("0.8.3.1")]
+[assembly: AssemblyFileVersion("0.8.3.1")]
