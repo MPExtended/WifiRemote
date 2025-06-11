14c792f63e9ca38a8d1ffc999c1d0afb5f7656f3
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-11 10:50:46 +0300

    Bump Windows to Windows Server 2022

--- Installer/Build_GIT.bat
+++ Installer/Build_GIT.bat
@@ -9,22 +9,26 @@ cd ..\Sources
 
 setlocal enabledelayedexpansion
 
-if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
-	:: 64-bit
-	set PROGS=%programfiles(x86)%
-	goto CONT
-:32BIT
-	set PROGS=%ProgramFiles%
-:CONT
-IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
-
 :: Prepare version
 for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
 set REVISION=%REVISION: =%
 "..\Installer\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" WifiRemote\Properties\AssemblyInfo.cs
 
 :: Build
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE /property:Platform="Any CPU" /fl /flp:logfile=WiFiRemote.log;verbosity=diagnostic WiFiRemote.sln
+FOR %%p IN ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") DO (
+  FOR %%s IN (2019 2022) DO (
+    FOR %%e IN (Community Professional Enterprise BuildTools) DO (
+      SET PF=%%p
+      SET PF=!PF:"=!
+      SET MSBUILD_PATH="!PF!\Microsoft Visual Studio\%%s\%%e\MSBuild\Current\Bin\MSBuild.exe"
+      IF EXIST "!MSBUILD_PATH!" GOTO :BUILD
+    )
+  )
+)
+
+:BUILD
+
+%MSBUILD_PATH% /target:Rebuild /property:Configuration=RELEASE /property:Platform="Any CPU" /fl /flp:logfile=WiFiRemote.log;verbosity=diagnostic WiFiRemote.sln
 
 : Revert version
 git checkout WifiRemote\Properties\AssemblyInfo.cs 
